77ac56c692ea233c53708f6cd543ccea09189771
KOLANICH <kolan_n@mail.ru>
2023-10-13 14:36:53 +0300

    We yad to move to Codeberg

--- BoringTun/__init__.py
+++ BoringTun/__init__.py
@@ -1,4 +1,8 @@
 __all__ = ("KeyPair", "Key", "PublicKey", "SecretKey", "Tunnel", "Opcode", "Action")
 
+from warnings import warn
+
+warn("We have moved from M$ GitHub to https://codeberg.org/KOLANICH-libs/BoringTUN.py , read why on https://codeberg.org/KOLANICH/Fuck-GuanTEEnomo .")
+
 from .KeyPair import Key, KeyPair, PublicKey, SecretKey
 from .Tunnel import Action, Opcode, Tunnel
